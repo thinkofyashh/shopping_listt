30aa36090c48a8e11da526f2c851cfb707605856
yashrawat <yashrawat682@gmail.com>
2023-08-09 18:43:05 +0530

    base setup.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_listt/widgets/grocery_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,17 +14,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Groceries',
       theme: ThemeData.dark().copyWith(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 147, 229, 250),
-              brightness: Brightness.dark,
-              surface: const Color.fromARGB(
-                255,
-                42,
-                51,
-                59,
-              ),),scaffoldBackgroundColor: const Color.fromARGB(255,50,58,60),),
-      home: const Scaffold(),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 147, 229, 250),
+          brightness: Brightness.dark,
+          surface: const Color.fromARGB(
+            255,
+            42,
+            51,
+            59,
+          ),
+        ),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
+      ),
+      home: const GroceryList(),
     );
   }
 }
